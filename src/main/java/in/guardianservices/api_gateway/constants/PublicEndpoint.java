161114d77f5d3e08fda4a3c9289305a454a9dd84
rahulsharma <rsharma0374@gmail.com>
2025-07-09 01:01:58 +0530

    updated new service

--- src/main/java/in/guardianservices/api_gateway/constants/PublicEndpoint.java
+++ src/main/java/in/guardianservices/api_gateway/constants/PublicEndpoint.java
@@ -22,7 +22,13 @@ public enum PublicEndpoint {
     EMAIL_GET_CURRENT_DAY_STATS("/email-connector/get-current-day-statistics", false, false),
     DOCUMENT_UTILITY_WELCOME("/doc-service/welcome", false, false),
     DOCUMENT_UTILITY_LOCK("/doc-service/pdf/lock", false, false),
-    DOCUMENT_UTILITY_UNLOCK("/doc-service/pdf/unlock", false, false);
+    DOCUMENT_UTILITY_UNLOCK("/doc-service/pdf/unlock", false, false),
+    DOCUMENT_UTILITY_PDF_TO_BASE64("/doc-service/pdf-to-base64", false, false),
+    DOCUMENT_UTILITY_BASE64_TO_PDF("/doc-service/base64-to-pdf", false, false),
+    DOCUMENT_UTILITY_PDF_COMPRESS("/doc-service/pdf/compress", false, false),
+    DOCUMENT_UTILITY_PDF_MERGE("/doc-service/pdf/merge", false, false),
+    DOCUMENT_UTILITY_PDF_SPLIT("/doc-service/pdf/split", false, false),
+    DOCUMENT_UTILITY_PDF_TO_IMAGES("/doc-service/pdf/to-images", false, false);
 
     @Getter
     private final String path;
