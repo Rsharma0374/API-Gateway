41dcb2eab31b7d47675b7bb1878d1bb089ac62fe
Rahul Sharma <rahulsharma@MacBookAir.lan>
2026-04-19 23:14:56 +0530

    removed all classes

--- src/main/java/in/guardianservices/api_gateway/ApiGatewayApplication.java
+++ src/main/java/in/guardianservices/api_gateway/ApiGatewayApplication.java
@@ -7,11 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableCaching
-//@EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
